1ddd76f89a284a16ebbda3d0105325152212bc1c
Kenza Daghrir <kdaghrir@dom.tti>
2022-07-20 19:44:13 +0100

    update work

--- Act6.1/Act6.1.2/README.md
+++ Act6.1/Act6.1.2/README.md
@@ -1,1 +1,8 @@
-# Module4
+les commandes qu'il faut lancer pour tester le projet
+1- composer install
+2- composer require server
+3-php bin/console server:run
+  les routes
+  "/articles"pour afficher tous les articles et les commentaires associés
+  "/addArticle" pour ajouter un article
+  "/delete" pour supprimer un article
--- Act6.1/Act6.1.2/src/Controller/ArticlesController.php
+++ Act6.1/Act6.1.2/src/Controller/ArticlesController.php
@@ -17,11 +17,64 @@ class ArticlesController extends AbstractController
     $response = $client->request('GET', '/posts');
     $body = $response->getBody();
     $articles = json_decode($body);
+
+
+    
+    $response = $client->request('GET', '/comments');
+    $body = $response->getBody();
+    $commentaires = json_decode($body);
  
         return $this->render('articles/index.html.twig', [
             'controller_name' => 'ArticlesController',
-                'articles' => $articles
+                'articles' => $articles ,'commentaires' => $commentaires
         ]);
+    }
+
+
+
+     
+/**
+     * @Route("/addArticle", name="app_add")
+     */
+    public function AddArticle()
+    { $client = new Client();
+        $response = $client->request('POST', 'https://jsonplaceholder.typicode.com/posts', [
+            'form_params' => [
+                'userId' => '1',
+                'id' => '101',
+                'title' => 'titre de cet article ajoute',
+                'body' => 'contenu ajouté',
+                ]
+        ]);
+        $body=$response->getBody();
+       
+        $code=$response->getStatusCode() ;
+        $reasonPhrase = $response->getReasonPhrase(); // OK
+     
+    return $this->render('articles/message.html.twig', [
+                    'controller_name' => 'ArticlesController',
+                    'code'=>$code ,
+                    'body'=>$body,
+                    'reasonPhrase'=>$reasonPhrase
+            ]);
+    } 
+
+    
+     /**
+     * @Route("/delete", name="app_delete")
+     */
+    public function DeleteArticle()
+     {
+        $client = new Client();
+        $response =$client->delete('https://jsonplaceholder.typicode.com/posts/1');
+        $code=$response->getStatusCode() ;
+        $reason = $response->getReasonPhrase(); // OK
+            return $this->render('articles/message.html.twig', [
+                    'controller_name' => 'ArticlesController',
+                    'code'=>$code ,
+                    'reasonPhrase'=>$reason,
+                    'body'=>''
+            ]);
     }
 }
 
