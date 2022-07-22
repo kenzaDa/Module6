1192545d519aebec921a39c119e982c2bbc26b04
Kenza Daghrir <kdaghrir@dom.tti>
2022-07-22 18:49:24 +0100

    act6.3

---
+++ Act6.3/src/Controller/ApiArticleController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+
+
+
+
+class ApiArticleController extends AbstractController
+{
+    /**
+     * @Route("/api/article", name="app_api_article")
+     */
+    public function index(): Response
+    {
+        return $this->render('api_article/index.html.twig', [
+            'controller_name' => 'ApiArticleController',
+        ]);
+    }
+    
+  /**
+     * @GET("/articles", name="article_list")
+     */
+    public function listAction(ArticleRepository $articlesRepo): Response
+    {
+
+    $articles = $articlesRepo->findAll();
+    $serializer = new Serializer(array(new DateTimeNormalizer('d.m.Y'), new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
+    $data = $serializer->serialize($articles, 'json');
+       // On instancie la réponse
+       $response = new Response($data);
+
+       // On ajoute l'entête HTTP
+       $response->headers->set('Content-Type', 'application/json');
+   
+       // On envoie la réponse
+       return $response;
+  }
+
+
+/**
+     * @Get(
+     *     path = "/article/{id}",
+     *     name = "app_article_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     */
+public function getArticle(Article $articles, ArticleRepository $articlesRepo,$id)
+{
+    $articles = $articlesRepo
+    ->find($id);
+    $serializer = new Serializer(array(new DateTimeNormalizer('d.m.Y'), new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
+    $data = $serializer->serialize($articles, 'json');
+       // On instancie la réponse
+       $response = new Response($data);
+
+       // On ajoute l'entête HTTP
+       $response->headers->set('Content-Type', 'application/json');
+   
+       // On envoie la réponse
+       return $response;
+}
+
+/**
+ * @POST("/article", name="ajout")
+ */
+public function addArticle(Request $request, SerializerInterface $serializer,EntityManagerInterface $em)
+{
+    $jsonRecu=$request->getContent();
+    $article=$serializer->deserialize($jsonRecu, Article::class,'json');
+    $em->persist($article);
+    $em->flush();
+    return $this->json($article,201,[]);
+}
+
+  /**
+
+     * @Put("/article/{id?}", name="edit")
+
+     */
+
+    public function editArticle(?Article $article, Request $request)
+
+    {
+
+            $donnees = json_decode($request->getContent());
+
+            $code = 200;
+
+            if(!$article){
+
+                $article = new Article();
+
+                $code = 201;
+
+            }
+
+            $article->setTitre($donnees->titre);
+
+            $article->setContenu($donnees->contenu);
+
+            $article->setAuteur($donnees->auteur);
+
+            $article->setDateDePublication (new \DateTime((string)$donnees->dateDePublication));
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+
+           return $this->json($article,$code,[]);
+
+    }
+  /**
+     * @GET("/lastarticle", name="article_list_three")
+     */
+    public function last3Articles(ArticleRepository $articlesRepo): Response
+    {
+
+    $articles = $articlesRepo->apiFindAll();
+    $serializer = new Serializer(array(new DateTimeNormalizer('d.m.Y'), new GetSetMethodNormalizer()), array('json' => new JsonEncoder()));
+    $data = $serializer->serialize($articles, 'json');
+       // On instancie la réponse
+       $response = new Response($data);
+
+       // On ajoute l'entête HTTP
+       $response->headers->set('Content-Type', 'application/json');
+   
+       // On envoie la réponse
+       return $response;
+  }
+
+  /**
+ * @DELETE("/article/{id}", name="supprime")
+ */
+public function removeArticle(Article $article ,EntityManagerInterface $em)
+{ if (!$article) {
+    return $this->json(["error message" => "article not found"],404);
+  } else {
+    $em ->remove($article);
+    $em->flush();
+    return new Response('ok');
+  }
+    
+
+    }}
+
---
+++ Act6.3/src/Entity/Article.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_de_publication;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getDateDePublication(): ?\DateTimeInterface
+    {
+        return $this->date_de_publication;
+    }
+
+    public function setDateDePublication(\DateTimeInterface $date_de_publication): self
+    {
+        $this->date_de_publication = $date_de_publication;
+
+        return $this;
+    }
+}
