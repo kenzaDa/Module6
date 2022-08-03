f752d11a009502c39eba0d95e02d34f11005e797
Kenza Daghrir <kdaghrir@dom.tti>
2022-08-03 09:30:50 +0100

    add a bundle

--- Act6.2/config/bundles.php
+++ Act6.2/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
